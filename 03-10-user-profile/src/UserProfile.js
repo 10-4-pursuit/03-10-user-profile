80f43b6ada18be8bc3a421753a92434d5761b21d
lanell1 <lanelllopez@pursuit.org>
2024-01-03 19:18:27 -0500

    complete-03-10-user-profile

---
+++ 03-10-user-profile/src/UserProfile.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const UserProfile = ({ name, location }) => {
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {name}</p>
+      <p>Location: {location}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
